aa871decce4312717157ee62b2bd3c6625b359d9
Mayank Maurya <mayankmaurya@LT-MBP-342.local>
2026-05-28 13:46:53 +0530

    added noida

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -28,6 +28,7 @@ const LOCATIONS = [
   { city: 'Ghaziabad', state: 'Uttar Pradesh', country: 'India' },
   { city: 'Delhi', state: 'Delhi', country: 'India' },
   { city: 'Lucknow', state: 'UttparPradesh', country: 'India' },
+  { city: 'Noida', state: 'UttparPradesh', country: 'India' },
 ];
 
 const SearchBar = ({ onSearch, initialFilters }: SearchBarProps) => {
